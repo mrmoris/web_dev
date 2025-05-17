ba3b8031924be171174ff753f2498af2133a545d
mrmoris <85683090+mrmoris@users.noreply.github.com>
2025-05-17 17:25:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+// async function myfun(){
+//     let p = await pms;
+//     console.log(p);
+// }
+// myfun();
 
+// 
+
+// const express = require('express');
+// const app = express();
+// const z = require('zod')
